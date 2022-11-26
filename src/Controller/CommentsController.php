4a9a2e6c6ccab67a6075d89ab821be8d6caa8d30
MOUATAZ HAKKOU <40317134+mouataz1@users.noreply.github.com>
2022-11-26 14:34:54 +0100

    comment controller

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Movie;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +21,46 @@ class CommentsController extends AbstractController
         return $this->render('comments/index.html.twig', [
             'controller_name' => 'CommentsController',
         ]);
+    }
+
+    /**
+     * @Route("/comments/add", name="app_add_comment", methods="POST")
+     */
+    public function addComment(EntityManagerInterface $em, Request $request):Response
+    {
+        try{
+            $movie = $em->getRepository(Movie::class)->find($request->get('mivie'));
+        $comment = new Comment();
+        $comment->setBody($request->get('comment'))
+                ->setLikes(0)
+                ->setDislikes(0)
+                ->setUser($em->getReference(User::class, $request->get('user')))
+                ->setMovie($movie);
+        $em->persist($movie);
+        $em->flush();
+        $this->addFlash('success', 'Comment Added');
+        return $this->redirectToRoute('app_comments');
+        }catch(\Exception $e){
+            $this->addFlash('error', 'an error occured please try again');
+            return $this->redirectToRoute('app_comments');
+        } 
+    }
+
+    /**
+     * @Route("/comment/remove", name="app_remove_comment", methods="POST")
+     */
+    public function removeComment(EntityManagerInterface $em, Request $request):Response
+    {
+        try{
+            $comment = $em->getRepository(Comment::class)->find($request->get('comment'));
+        $em->remove($comment);
+        $em->flush();
+        $this->addFlash('success', 'comment Removed !');
+        return $this->redirectToRoute('app_comments');
+        }catch(\Exception $e){
+            $this->addFlash('error', 'an error occured please try again');
+            return $this->redirectToRoute('app_comments');
+        } 
+        
     }
 }
