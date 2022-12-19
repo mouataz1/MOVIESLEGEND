70ce97d22b52fabdf00364d99ed05ac232f13d12
MOUATAZ HAKKOU <40317134+mouataz1@users.noreply.github.com>
2022-12-19 19:32:35 +0100

    contact controller

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +19,22 @@ class ContactController extends AbstractController
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
+    }
+
+    /**
+     * @Route("/contact/add", name="app_add_contact", methods="POST")
+     */
+    public function addContact(EntityManagerInterface $em, Request $request):Response
+    {
+        $contact = new Contact();
+        $contact->setFirstName($request->get("fname"))
+                ->setLastName($request->get("lname"))
+                ->setEmail($request->get("email"))
+                ->setSubject($request->get('subject'))
+                ->setMessage($request->get('message'));
+        $em->persist($contact);
+        $em->flush();
+        $this->addFlash('success', 'Thnak You, we\'ll contact you bak as soon as possible .');
+        return $this->redirect("app_home");
     }
 }
